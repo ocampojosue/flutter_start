5eca68cb78808bac9a2cb3b3bd6c5f3a03ba19f1
ocampojosue <ocampojosue08@gmail.com>
2023-02-06 16:21:35 -0400

    Navegar entre varias pantallas

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: deprecated_member_use
 
+import 'package:api_rest_yt_course/pages/page02.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -30,111 +31,31 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: cuerpo(),
+      appBar: AppBar(
+        title: const Text("NAVEGACIÓN"),
+        backgroundColor: Colors.red,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text("HOME"),
+            ElevatedButton(
+              onPressed: () => {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: ((context) => const Page02()))),
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.red,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              child: const Text("IR A SIGUIENTE PANTALLA"),
+            )
+          ],
+        ),
+      ),
     );
   }
-}
-
-Widget cuerpo() {
-  return Container(
-    decoration: const BoxDecoration(
-      image: DecorationImage(
-        image: NetworkImage(
-          "https://img.freepik.com/vector-premium/montanas-paisaje-forestal-bello-cielo-nocturno-estrellas_16058-112.jpg?w=740",
-        ),
-        fit: BoxFit.cover,
-      ),
-    ),
-    child: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          nombre(),
-          campousuario(),
-          campocontrasena(),
-          const SizedBox(
-            height: 20.0,
-          ),
-          btnEntrar(),
-        ],
-      ),
-    ),
-  );
-}
-
-Widget nombre() {
-  return const Text(
-    "INICIAR SESIÓN",
-    style: TextStyle(
-      color: Colors.white,
-      fontSize: 40.0,
-      fontFamily: "Monospace",
-    ),
-  );
-}
-
-Widget campousuario() {
-  return Container(
-    padding: const EdgeInsets.symmetric(
-      horizontal: 40.0,
-      vertical: 10.0,
-    ),
-    child: const TextField(
-      decoration: InputDecoration(
-        hintText: "NOMBRE DE USUARIO",
-        fillColor: Colors.white,
-        filled: true,
-      ),
-    ),
-  );
-}
-
-Widget campocontrasena() {
-  return Container(
-    padding: const EdgeInsets.symmetric(
-      horizontal: 40.0,
-      vertical: 10.0,
-    ),
-    child: const TextField(
-      obscureText: true,
-      decoration: InputDecoration(
-        hintText: "CONTRASEÑA",
-        fillColor: Colors.white,
-        filled: true,
-      ),
-    ),
-  );
-}
-
-Widget btnEntrar() {
-  return ElevatedButton(
-    // style: ButtonStyle(
-    //   shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-    //     RoundedRectangleBorder(
-    //         borderRadius: BorderRadius.circular(18.0),
-    //         side: BorderSide(color: Colors.blueAccent)),
-    //   ),
-    // ),
-    style: ElevatedButton.styleFrom(
-      primary: Colors.blue,
-      onPrimary: Colors.black,
-      padding: const EdgeInsets.symmetric(
-        horizontal: 30,
-        vertical: 10,
-      ),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(10.0),
-      ),
-    ),
-
-    onPressed: () {},
-    child: const Text(
-      "ENTRAR",
-      style: TextStyle(
-        fontSize: 20.0,
-        color: Colors.white,
-        fontFamily: "Monospace",
-      ),
-    ),
-  );
 }
---
+++ lib/pages/page02.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class Page02 extends StatelessWidget {
+  const Page02({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("TÉRMINOS Y CONDICIONES"),
+        backgroundColor: Colors.red,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: <Widget>[
+            const Text(
+              "TÉRMINOS Y CONDICIONES",
+              style: TextStyle(
+                fontSize: 25.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              "Para usar esta aplicación es necesario que aceptes los términos y condiciones. Para usar esta aplicación es necesario que aceptes los términos y condiciones. Para usar esta aplicación es necesario que aceptes los términos y condiciones",
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+              textAlign: TextAlign.justify,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Text(
+              "Para usar esta aplicación es necesario que aceptes los términos y condiciones. Para usar esta aplicación es necesario que aceptes los términos y condiciones. Para usar esta aplicación es necesario que aceptes los términos y condiciones",
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+              textAlign: TextAlign.justify,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Text(
+              "Para usar esta aplicación es necesario que aceptes los términos y condiciones. Para usar esta aplicación es necesario que aceptes los términos y condiciones. Para usar esta aplicación es necesario que aceptes los términos y condiciones",
+              style: TextStyle(
+                fontSize: 16.0,
+              ),
+              textAlign: TextAlign.justify,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            ElevatedButton(
+              onPressed: () => {
+                Navigator.pop(context),
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                padding: const EdgeInsets.symmetric(
+                  vertical: 15.0,
+                  horizontal: 10.0,
+                ),
+                primary: Colors.red,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const <Widget>[
+                  Text(
+                    "ACEPTO TODO",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
